e823724406f7e1d8c78a8e0bb9c5f8a1dfd70f62
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-01 06:52:25 +0100

    Reading flavour from user and storing that into flavours array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace Ice_Cream_Algorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] flavours = new string[10];
+             int i = 0;
+            while (i < 10)
+                {
+
+                    Console.WriteLine("Select your favourite flavour:");
+                    Console.WriteLine("1) Chocolate");
+                    Console.WriteLine("2) Banana");
+                    Console.WriteLine("3) Vanilla");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            flavours[i] = "Chocolate";
+                            break;
+                        case 2:
+                            flavours[i] = "Banana";
+                            break;
+                        case 3:
+                            flavours[i] = "Vanilla";
+                            break;
+                    }
+                    i++;
+
+
+                }
         }
     }
 }

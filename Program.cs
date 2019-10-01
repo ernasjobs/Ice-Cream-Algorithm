2a4182373083cae1a2ea90391a93c488873216b1
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-01 06:58:54 +0100

    Check if all element of flavours array are all equal

--- Program.cs
+++ Program.cs
@@ -6,32 +6,53 @@ namespace Ice_Cream_Algorithm
     {
         static void Main(string[] args)
         {
+            int j;
+            bool allEqual = false;
             string[] flavours = new string[10];
-             int i = 0;
-            while (i < 10)
+             
+             do
+             {
+                    int i = 0;
+                    while (i < 10)
+                   {
+                        Console.WriteLine("Select your favourite flavour:");
+                        Console.WriteLine("1) Chocolate");
+                        Console.WriteLine("2) Banana");
+                        Console.WriteLine("3) Vanilla");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                flavours[i] = "Chocolate";
+                                break;
+                            case 2:
+                                flavours[i] = "Banana";
+                                break;
+                            case 3:
+                                flavours[i] = "Vanilla";
+                                break;
+                                 
+                        }
+                        i++;
+                    }  
+                    allEqual = false;
+                for ( j = 1; j <flavours.Length; j++)
                 {
-
-                    Console.WriteLine("Select your favourite flavour:");
-                    Console.WriteLine("1) Chocolate");
-                    Console.WriteLine("2) Banana");
-                    Console.WriteLine("3) Vanilla");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case 1:
-                            flavours[i] = "Chocolate";
-                            break;
-                        case 2:
-                            flavours[i] = "Banana";
-                            break;
-                        case 3:
-                            flavours[i] = "Vanilla";
-                            break;
-                    }
-                    i++;
-
-
+                    if (flavours[0] == flavours[j])
+                        continue;
+                    else
+                        break;
                 }
+                if (j== flavours.Length)
+                {
+                    allEqual = true;
+                }
+
+                  
+            } while(!allEqual);
+                
+             
+           
         }
     }
 }

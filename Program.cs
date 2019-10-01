6004649e5760f6437ce5be29bb9c1f178b1b2c5b
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-01 06:47:37 +0100

    C# Console App is created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ice_Cream_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
